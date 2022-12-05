9c6e0b16e637784fdfa2f51b57295b14a26babaf
Joao Felipe <jfcbxp@gmail.com>
2022-12-05 10:35:21 -0300

    adicionado funcoes de limpar e iniciar

--- App.tsx
+++ App.tsx
@@ -1,11 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 
 export default function App() {
+  const [numero, setNumero] = useState<number>(0);
+  const [timer, setTimer] = useState<NodeJS.Timer>();
+  const [ativo, setAtivo] = useState<boolean>(false);
+  const [ultimoTempo, setUltimoTempo] = useState<number>(0);
+
+  const limpar = (): void => {
+    clearInterval(timer);
+    setAtivo(false);
+    setUltimoTempo(numero);
+    setNumero(0);
+  };
+
+  const iniciar = (): void => {
+    setTimer(
+      setInterval(() => {
+        setNumero((numero) => numero + 0.1);
+      }, 100)
+    );
+    setAtivo(true);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require("./assets/cronometro.png")} />
+      <Text style={styles.timer}>{numero?.toFixed(1)}</Text>
+      <View style={styles.btnArea}>
+        <TouchableOpacity style={styles.btn} onPress={iniciar} disabled={ativo}>
+          <Text style={styles.btnTexto}>Vai</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btn} onPress={limpar} disabled={!ativo}>
+          <Text style={styles.btnTexto}>Limpar</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.areaUltima}>
+        <Text style={styles.textoCorrida}>
+          {ultimoTempo > 0 ? `Ultimo tempo:${ultimoTempo?.toFixed(1)}` : ""}
+        </Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +48,41 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#00aeef",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  timer: {
+    marginTop: -160,
+    color: "#FFF",
+    fontSize: 65,
+    fontWeight: "bold",
+  },
+  btnArea: {
+    flexDirection: "row",
+    marginTop: 70,
+    height: 40,
+  },
+  btn: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FFF",
+    height: 40,
+    margin: 17,
+    borderRadius: 9,
+  },
+  btnTexto: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#00aeef",
+  },
+  areaUltima: {
+    marginTop: 40,
+  },
+  textoCorrida: {
+    fontSize: 25,
+    fontStyle: "italic",
+    color: "#FFF",
   },
 });
